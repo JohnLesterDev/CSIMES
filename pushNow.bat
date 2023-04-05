0ffbfbec62ade26a59ffa99cc21f5d1b21f61545
JohnLesterDev <johnlesterincbusiness@gmail.com>
2023-04-05 16:29:01 +0800

    Modifying Project Files

---
+++ pushNow.bat
@@ -0,0 +1,16 @@
+:: Written by JohnLesterDev :>
+
+@echo off
+
+echo [CSIMES] Building the Jar file...
+call gradle build -x test
+
+echo [CSIMES] Commiting changes with message (%1)...
+git add .
+git commit -m "%~1"
+
+echo Pushing to remote main...
+git push origin main
+
+echo All processes done!
+pause
